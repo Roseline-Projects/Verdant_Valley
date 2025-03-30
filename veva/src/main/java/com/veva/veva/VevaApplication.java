0c15a0bcf4498cc654ed95aad584e46549914925
Roseline <rdami007@fiu.edu>
2025-03-30 16:40:28 -0400

    init environment

---
+++ veva/src/main/java/com/veva/veva/VevaApplication.java
@@ -0,0 +1,13 @@
+package com.veva.veva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VevaApplication.class, args);
+	}
+
+}
---
+++ veva/src/test/java/com/veva/veva/VevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veva.veva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
